03df556dc1b90d11998731bcad0791c3fe77b5a2
Renzo Anaya <r.anayaa@alum.up.edu.pe>
2022-06-30 23:30:12 -0500

    commit prenormalize

--- desafioNorm.js
+++ desafioNorm.js
@@ -4,17 +4,21 @@ import {inspect} from 'util';
 const mensajesOriginal = {};
 
 
-const mensajesSchema = new schema.Entity('mensajes')
+const autoresSchema = new schema.Entity('autores');
+const mensajesSchema = new schema.Entity('mensajes',{
+    authorName: userSchema,
+    text:[commentSchema]
+})
 
-const autoresSchema = new schema.Entity('autores')
 
 
 
 
-const autoresNormalized = normalize(mensajesOriginal, autoresSchema);
+
+const mensajesNormalized = normalize(mensajesOriginal, mensajesSchema);
 
 function print(objeto){
     console.log(inspect(objeto,false,12,true))
 };
 
-print(autoresNormalized)
+print(mensajesNormalized)
